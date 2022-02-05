2d66dfcda1ca5b88cf04360cc4b8d701e6ddee44
VertexPlays <90276412+TheMrStealth@users.noreply.github.com>
2022-02-04 21:07:35 -0500

    Made big changes to home page

--- DBC2/DBC2.js
+++ DBC2/DBC2.js
@@ -23,7 +23,7 @@ const renderGameBaseBorders = (borderWidth) => {
 
 const {game, margin} = renderGameBaseBorders(0)
 
-game.style.backgroundImage = "url('../images/dbc2background.png')"
+game.style.backgroundImage = "url('./DBC2images/dbc2background.png')"
 game.style.backgroundSize = "cover"
 
 const Power = document.createElement("h1")
@@ -37,7 +37,7 @@ margin.style.justifyContent = "space-between"
 margin.style.alignItems = "center"
 
 const Goku = document.createElement("img")
-Goku.src = "../images/goku.png"
+Goku.src = "./DBC2images/goku.png"
 Goku.height = 625
 Goku.width = 450
 
